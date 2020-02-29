7be39a0983af974beda670aca20c954f5d97884e
VarianM <mizeyuyu@gmail.com>
2020-02-29 23:08:28 +0800

    使用svgo-loader解决了SVG自带fill属性的问题

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,12 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({
+        ...options,
+        plugins: [{removeAttrs: {attrs: 'fill'}}]
+      })) // svg优化插件：发现加载了svg就删除svg中的fill属性（不然颜色可能会变）
+      .end()
 
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 让其他 svg loader 排除 icons 目录
