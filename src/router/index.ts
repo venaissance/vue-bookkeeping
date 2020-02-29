8a2f31c0d0bde700508f59ec44091f3c3bb5871d
VarianM <mizeyuyu@gmail.com>
2020-02-29 17:17:48 +0800

    添加了VueRouter 404页面，并删除初始Home，About页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '@/views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -23,6 +23,10 @@ const routes = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFound
   }
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>你输入的地址有误，请重新输入</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
