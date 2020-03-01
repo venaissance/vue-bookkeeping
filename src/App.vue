2878b4e0804e17152fe334dbb79451992d766255
VarianM <mizeyuyu@gmail.com>
2020-03-01 23:34:30 +0800

    完成Money页的HTML和CSS

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,16 @@
 </template>
 
 <style lang="scss">
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+  @import "~@/assets/styles/helper.scss";
+  @import "~@/assets/styles/reset.scss";
+
   body {
     line-height: 1.5;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: $font-hei;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    font-size: 14px;
+    color: #333;
   }
 
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
 </style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
       <slot/>
     </div>
     <Nav/>
@@ -9,12 +9,13 @@
 
 <script lang="ts">
   export default {
+    props: ['classPrefix'],
     name: 'Layout'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
+  .layout-wrapper {
     display: flex;
     flex-direction: column;
     height: 100vh;
