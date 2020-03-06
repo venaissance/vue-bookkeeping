385353462aaab94d5434724f1c98ae63ff2c383a
VarianM <mizeyuyu@gmail.com>
2020-03-06 23:03:24 +0800

    增加 Labels 页标签点击跳转编辑页面功能

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,14 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag"
+                   v-for="tag in tags"
+                   :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -44,7 +47,7 @@
     font-size: 16px;
     padding-left: 16px;
 
-    li {
+    .tag {
       display: flex;
       justify-content: space-between;
       align-items: center;
