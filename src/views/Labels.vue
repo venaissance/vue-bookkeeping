0487f73d2dbe28956bbc6e39aa4c17a847b9f601
VarianM <mizeyuyu@gmail.com>
2020-03-06 21:12:59 +0800

    完成 Labels 页的 HTML 和 CSS

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,14 @@
 <template>
   <Layout>
-    Label.vue
+    <ol class="tags">
+      <li>
+        <span>吃</span>
+        <Icon name="right"></Icon>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
   </Layout>
 </template>
 
@@ -11,4 +19,41 @@
   };
 </script>
 
+<style lang="scss" scoped>
+  .tags {
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
 
+    li {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px 30px;
+      border-bottom: 1px solid #e6e6e6;
+      min-height: 44px;
+    }
+
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+
+  .createTag {
+    border-radius: 4px;
+    border: none;
+    background: #767676;
+    color: white;
+    padding: 0 16px;
+    height: 40px;
+
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 44-16px;
+    }
+  }
+</style>
