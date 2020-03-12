ffbbbcc3f92d33160f2691563acce6c0498eec8e
VarianM <mizeyuyu@gmail.com>
2020-03-12 22:47:46 +0800

    为提高 PC 用户体验，添加扫码预览功能

--- src/main.ts
+++ src/main.ts
@@ -27,3 +27,16 @@ window.onload = function () {
     window.scrollTo(0, 200);
   }, 0);
 };
+
+// PC用户提示使用手机扫码预览
+if (document.documentElement.clientWidth > 500) {
+  window.alert('最佳效果需要使用手机扫码查看哦~');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%, -50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
